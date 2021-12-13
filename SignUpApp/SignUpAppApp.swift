1a54fdc17f1b1d89880507e0181dc92512809c52
AlexMochalov <ilold2191@gmail.com>
2021-12-13 18:56:13 +0200

    Initial Commit

---
+++ SignUpApp/SignUpAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpAppApp.swift
+//  SignUpApp
+//
+//  Created by Alex Mochalov on 13/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SignUpAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
